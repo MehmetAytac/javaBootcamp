740a28c3ea82e6c2d2b28fecb548f2739fe89aa5
mehme <mehme@DESKTOP-69TG8P4.home>
2022-09-21 16:11:34 +0300

    vowels

---
+++ vowels/src/vowels/vowels.java
@@ -0,0 +1,22 @@
+package vowels;
+
+public class vowels {
+
+	public static void main(String[] args) {
+		
+char letter = 'İ';
+		
+		switch (letter) {
+		case 'A':
+		case 'I':
+		case 'O':
+		case 'U':
+			System.out.println("Kalın Sesli harf");
+			break;
+			default:
+			System.out.println("İnce Sesli");
+		}
+
+	}
+
+}
